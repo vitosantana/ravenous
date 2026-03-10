a7c53c957bb769a2215c1ff25bfa950ada16fe3a
Devoughn Huggans <devoughnhuggans@gmail.com>
2026-03-10 17:09:48 -0500

    Completed SearchBar state and simulated search

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,19 +1,62 @@
+import { useState } from 'react';
 import styles from './SearchBar.module.css';
 
 function SearchBar() {
+    const [term, setTerm] = useState('');
+    const [location, setLocation] = useState('');
+    const [sortBy, setSortBy] = useState('best_match');
+
+    const handleSortByChange = (sortOption) => {
+        setSortBy(sortOption);
+    };
+
+    const handleTermChange = (event) => {
+        setTerm(event.target.value);
+    };
+
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    };
+
+    const handleSearch = () => {
+        console.log(`Searching Yelp with ${term}, ${location}, ${sortBy} `);
+    };
     return (
         <div className={styles.searchBar}>
             <div className={styles.sortOptions}>
-                <button className={styles.active}>Best Match</button>
-                <button>Highest Rated</button>
-                <button>Most Reviewed</button>
+                <button 
+                className={sortBy === "best_match" ? styles.active : ""}
+                onClick={() => handleSortByChange("best_match")}
+                >
+                Best Match
+                </button>
+                <button 
+                className={sortBy === "rating" ? styles.active : ""}
+                onClick={() => handleSortByChange("rating")}
+                >
+                Highest Rated
+                </button>
+                  <button 
+                className={sortBy === "review_count" ? styles.active : ""}
+                onClick={() => handleSortByChange("review_count")}
+                >
+                Most Reviewed
+                </button>
                 </div>
 
         <div className={styles.fields}>
-        <input placeholder="Search Businesses" />
-        <input placeholder="Where?" />
+        <input 
+        placeholder="Search Businesses"
+        value={term}
+        onChange={handleTermChange}
+        />
+        <input 
+        placeholder="Where?"
+        value={location}
+        onChange={handleLocationChange}
+         />
         </div>
-        <button className={styles.searchButton}>Let's Go</button>
+        <button className={styles.searchButton}onClick={handleSearch}>Let's Go</button>
         </div>
     );
 }
